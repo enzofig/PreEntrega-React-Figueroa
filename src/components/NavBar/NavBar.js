9daf11ebf8a2dedc7ba651c92b5eeb9f10caa8aa
enzofig <enzofigue4777@gmail.com>
2023-09-12 14:30:08 -0300

    agregar esto

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,16 @@
+import CartWidget from "../CartWidget/CartWidget"
+const NavBar = () => {
+    return (
+        <nav>
+            <h3>Camisetas</h3>
+        
+        <div>
+<button>Camiseta</button>
+<button>Color</button>
+<button>Numero</button>
+        </div>
+        <CartWidget />
+        </nav>
+    )
+}
+export default NavBar
