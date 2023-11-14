ac01ac7834c9993398440aa2e755070a0698921f
enzofig <enzofigue4777@gmail.com>
2023-11-14 13:14:57 -0300

    correcion nueva entrega

--- src/App.js
+++ src/App.js
@@ -1,33 +1,32 @@
-
 import './App.css';
 import NavBar from './components/NavBar/NavBar';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
 import ItemCount from './components/ItemCount/ItemCount';
-import ItemDetailContainer from './components/ItemDetailContainer/ItemDetailContainer'
-import Cart from './components/Cart/Cart'
-import Checkout from './components/Checkout/Checkout'
+import ItemDetailContainer from './components/ItemDetailContainer/ItemDetailContainer';
+import Cart from './components/Cart/Cart';
+import Checkout from './components/Checkout/Checkout';
 
 import { CartProvider } from './context/CartContext';
-import { db } from "./config/firebase";
-import { collection, getDocs } from "firebase/firestore";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-      <CartProvider>
-   <NavBar />
-   <Routes>
-    <Route path='/' element={<ItemListContainer greeting={'Todos nuestros productos'}/>}/>
-    <Route path='/category/categoryId' element={<ItemListContainer greeting={'Productos por categoria'}/>} />
-    <Route path='/item/:itemId' element={ <ItemDetailContainer />}/>
-    <Route path='/cart' element={<Cart />} />
-    <Route path='/checkout' element={<Checkout />} />
-    <Route path='*' element={<h1>404 NOT FOUND</h1>} />
-  </Routes>
-  </BrowserRouter>
- 
-   <ItemCount initial={1} stock={10} onAdd={(quantity) => console.log('cantidad agregada',quantity)}/>
-   <ItemDetailContainer />
+        <CartProvider>
+          <NavBar />
+          <Routes>
+            <Route path='/' element={<ItemListContainer greeting={'Todos nuestros productos'} />} />
+            <Route path='/category/categoryId' element={<ItemListContainer greeting={'Productos por categoria'} />} />
+            <Route path='/item/:itemId' element={<ItemDetailContainer />} />
+            <Route path='/cart' element={<Cart />} />
+            <Route path='/checkout' element={<Checkout />} />
+            <Route path='*' element={<h1>404 NOT FOUND</h1>} />
+          </Routes>
+
+          <ItemCount initial={1} stock={10} onAdd={(quantity) => console.log('cantidad agregada', quantity)} />
+        </CartProvider>
+      </BrowserRouter>
     </div>
   );
 }
